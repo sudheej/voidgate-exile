1fea073322cb3cc6b7ee6c5dbe0b34a797eba489
sudheej sudhakaran <sudheej@live.com>
2022-09-18 00:07:22 -0400

    (wip) prettier and husky added code cleanup

---
+++ src/components/Inventory.js
@@ -0,0 +1,26 @@
+import Weapon from "./Weapon";
+import { weaponarray } from "../state/WeaponArray";
+
+export default function Inventory(scene) {
+  this.scene = scene;
+}
+
+Inventory.prototype.createInvetory = function () {
+  let WeaponProperties = {
+    _id: "weapontimele",
+    x: 875,
+    y: 210,
+    height: 25,
+    width: 25,
+    type: "basic_gun",
+  };
+
+  const weapon = new Weapon(this.scene, WeaponProperties);
+
+  weapon.createTile();
+
+  weapon.rectangle.setInteractive();
+  weapon.rectangle.on("pointerdown", () => {
+    weaponarray.selectedproperty = weapon;
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,64 +1,37 @@
-import Phaser from 'phaser';
-import Tile from './components/Tile'
-import Map from './components/Map'
-import Weapon from './components/Weapon';
-import plainmap from './assets/maps/plainfield.json'
-import { weaponarray } from './state/WeaponArray';
+import Phaser from "phaser";
+import Tile from "./components/Tile";
+import Map from "./components/Map";
+import Weapon from "./components/Weapon";
+import plainmap from "./assets/maps/plainfield.json";
+import { weaponarray } from "./state/WeaponArray";
+import Inventory from "./components/Inventory";
 
+class Main extends Phaser.Scene {
+  constructor() {
+    super();
+  }
 
-class Main extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
+  preload() {}
 
-    preload ()
-    {
-    
-    }
-      
-    create ()
-    {
-
-        
-
-        this.input.setDefaultCursor('url(assets/cursors/cur.cur), cursor')
-        const map = new Map(this);
-        map.MappingData = plainmap;
-        map.createMap(100,100);
-
-
-            let WeaponProperties = {
-                _id: "weapontimele",
-                x: 875,
-                y: 210,
-                height: 25,
-                width: 25,
-                type: "basic_gun"
-            };
-            
-            const weapon = new Weapon(this,WeaponProperties);
-                        
-            weapon.createTile()
-
-            weapon.rectangle.setInteractive();
-            weapon.rectangle.on('pointerdown', () => { console.log('pointerover'); weaponarray.selectedproperty = weapon});
-      
-           
-
-    }
+  create() {
+    this.input.setDefaultCursor("url(assets/cursors/cur.cur), cursor");
+    const map = new Map(this);
+    map.MappingData = plainmap;
+    map.createMap(100, 100);
+    const inventory = new Inventory(this);
+    inventory.createInvetory();
+  }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#000000',
-    parent: 'game',
-    width: 1000,
-    height: 800,
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    scene: Main
+  type: Phaser.AUTO,
+  backgroundColor: "#000000",
+  parent: "game",
+  width: 1000,
+  height: 800,
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  scene: Main,
 };
 
 const game = new Phaser.Game(config);
