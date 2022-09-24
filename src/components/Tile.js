5cd94153518ba719f9718b587f841786ce30408f
sudheej sudhakaran <sudheej@live.com>
2022-09-24 12:09:51 -0400

    end tile added

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -14,12 +14,14 @@ export default function Tile(scene, tileproperties) {
   this.tileproperties = tileproperties;
 }
 
+//   graphics.fillGradientStyle(0xffff00, 0xff0000, 0xffff00, 0xff0000, 1);
+
 Tile.prototype.createTile = function () {
   this.rectangle.setStrokeStyle(1);
   let entryGradient = new Phaser.GameObjects.Graphics(this.scene);
   if (this.tileproperties.type === "path") {
     this.rectangle.setFillStyle(0x000000);
-  } else if (this.tileproperties.type === "start_path") {
+  } else if (this.tileproperties.type === "start_path_top") {
     this.rectangle.fillColor = 0x000000;
     entryGradient.fillGradientStyle(0x7cfc00, 0xadff2f, 0x000000, 0x000000, 1);
     entryGradient.fillRect(
@@ -29,7 +31,18 @@ Tile.prototype.createTile = function () {
       this.tileproperties.height
     );
     this.scene.add.existing(entryGradient);
-  } else if (this.tileproperties.type === "basic_gun") {
+  } else if (this.tileproperties.type === "end_path_right") {
+    this.rectangle.fillColor = 0x000000;
+    entryGradient.fillGradientStyle(0x000000, 0xff0000, 0x000000, 0xff0000, 1);
+    entryGradient.fillRect(
+      this.tileproperties.x - 12  ,
+      this.tileproperties.y - 10,
+      this.tileproperties.width,
+      this.tileproperties.height
+    );
+    this.scene.add.existing(entryGradient);
+  } 
+  else if (this.tileproperties.type === "basic_gun") {
     this.rectangle.fillColor = 0x328720;
   } else {
     this.scene.add.existing(this.rectangle);
