027aef9765b21959db042d64ed4e16d16ca03a36
sudheej sudhakaran <sudheej@live.com>
2023-10-28 21:23:12 -0400

    rudimentry version of font text

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,29 +1,40 @@
 import { gameStore } from "../state/GameStore";
 import { autorun } from "mobx";
 import { observer } from "mobx-react";
+import WebFont from 'webfontloader';
+
 export class ScoreBoard extends Phaser.GameObjects.Container {
   constructor(scene, x, y, lives, money) {
     super(scene, x, y);
 
+    // Add the Orbitron font to the project
+    WebFont.load({
+      google: {
+        families: ['Orbitron']
+      },
+      active: function() {
+        // Font is loaded and ready to use
+      }
+    });
+
     // Create text objects to display lives and money
-    //this.livesText = scene.add.text(0, 0, `Lives: ${lives}`, { fontSize: '24px', fill: '#FFF' });
     this.moneyText = scene.add.text(0, 30, `Money: $${gameStore.money}`, {
-      fontSize: "24px",
-      fill: "#FFF",
+      fontSize: "20px",
+      fill: "#4DD4CA",
+      fontFamily: "Orbitron"
     });
 
     this.waveText = scene.add.text(10, 30, `Wave: ${gameStore.wave}`, {
-      fontSize: "24px",
-      fill: "#FFF",
+      fontSize: "20px",
+      fill: "#4DD4CA",
+      fontFamily: "Orbitron"
     });
 
     // Add the text objects to the container
-    //this.add(this.livesText);
     this.add(this.moneyText);
 
     // Add the container to the scene
-
-    this.setSize(200, 50);
+    //this.setSize(200, 50);
     this.setX(800);
     scene.add.existing(this);
 
